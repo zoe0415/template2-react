ee139c2f537861d9f9184147ac94194149eff6b1
zoe <zoe@zoedeMacBook-Air.local>
2021-11-28 23:49:16 +0800

    調整router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-// import logo from "./logo.svg"
-// import "./App.css"
-
-// ---------------------------------------------------------------------------
-// router
-import { Route } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 
 // fontawesome
 import { library } from "@fortawesome/fontawesome-svg-core"
@@ -23,6 +18,8 @@ import {
   faChevronDown,
   faChevronUp,
   faBars,
+  faTrashAlt,
+  faKey,
 } from "@fortawesome/free-solid-svg-icons"
 import { faStar as farStar, faCopyright } from "@fortawesome/free-regular-svg-icons"
 library.add(
@@ -42,33 +39,41 @@ library.add(
   faTimes,
   faChevronDown,
   faChevronUp,
-  faBars
+  faBars,
+  faTrashAlt,
+  faKey
 )
 
 // components
-// import { Books, Electronics } from "./pages/Page"
 import Header from "./components/layout/Header"
 import Footer from "./components/layout/Footer"
+import TopBtn from "./components/layout/TopBtn"
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
 import Shop from "./pages/Shop"
 import Product from "./pages/Product"
 import Cart from "./pages/Cart"
-import TopBtn from "./components/layout/TopBtn"
-// import Register from "./pages/Register"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <main>
       <Header></Header>
       <TopBtn></TopBtn>
-      <section className="px-8 pt-28">
-        <Route path="/" component={Home} exact />
-        {/* <Route path="/blog/:type?" component={Blog} /> */}
-        <Route path="/blog" component={Blog} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/product" component={Product} />
-        <Route path="/cart" component={Cart} />
+      <section className="px-8 pt-28 pb-8">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/blog/:type?" component={Blog} />
+          {/* <Route path="/blog" component={Blog} /> */}
+          <Route path="/shop" component={Shop} />
+          <Route path="/product" component={Product} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          {/* <Route component={Home} />   404回到home，不會更改url*/}
+          <Redirect to="/" />
+        </Switch>
       </section>
       <Footer></Footer>
     </main>
--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 // import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter, Switch } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 import { Provider } from "react-redux"
 import { createStore } from "redux"
-import counter from "./reducers"
-const store = createStore(counter)
+// import counter from "./reducers"
+// const store = createStore(counter)
+import rootReducer from "./redux/reducers"
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Provider store={store}>
-          <App />
-        </Provider>
-        {/* <App /> */}
-      </Switch>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
