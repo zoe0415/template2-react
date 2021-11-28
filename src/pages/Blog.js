34da4e1bb81b10b668c6bbb3a53f3d23ccd8a4fd
zoe <zoe@zoedeMacBook-Air.local>
2021-11-28 23:52:05 +0800

    篩選商品

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import { useSelector } from "react-redux"
 import Sidebar from "../components/layout/Sidebar"
@@ -9,28 +9,47 @@ function Blog() {
   const useQuery = () => new URLSearchParams(useLocation().search)
   const query = useQuery()
   // data
-  const [blogList] = useState(useSelector((state) => state.blogDate))
+  const [queryString, setQueryString] = useState(query.get("type"))
+  const allBlogList = useSelector((state) => state.blog)
 
+  // useEffect
   useEffect(() => {
-    console.log(query.get("type"))
-  })
+    setQueryString(query.get("type"))
+  }, [query, allBlogList])
+
+  // function
+  const filterProducts = (queryString, allBlogList) => {
+    if (queryString == "All" || !queryString) {
+      return allBlogList
+    } else {
+      return allBlogList.filter((item) => {
+        return item.tag.includes(queryString)
+      })
+    }
+  }
+  const queryBlog = filterProducts(queryString, allBlogList)
 
   return (
-    <section className="flex gap-x-4">
+    <section className="flex gap-x-16">
       <section className="hidden lg:block w-1/5">
         <Sidebar></Sidebar>
       </section>
-      <section className="w-full lg:w-4/5 grid grid-cols-4 gap-x-4 gap-y-8">
-        {blogList.map((item, index) => (
-          <BlogCard
-            key={index}
-            title={item.title}
-            describe={item.describe}
-            date={item.date}
-            img={item.img}
-            tag={item.tag}
-          ></BlogCard>
-        ))}
+      <section className="w-full lg:w-4/5">
+        <h3 className="pb-4 mb-8 text-3xl font-semibold border-b border-gray-light capitalize">
+          {queryString ? queryString : "Blog"}
+        </h3>
+        <section className="grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-x-4 gap-y-8">
+          {queryBlog.map((item, index) => (
+            <BlogCard
+              key={index}
+              title={item.title}
+              describe={item.describe}
+              date={item.date}
+              img={item.img}
+              tag={item.tag}
+            ></BlogCard>
+          ))}
+        </section>
       </section>
     </section>
   )
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,25 +1,60 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
 import { useSelector } from "react-redux"
 import SidebarProduct from "../components/layout/SidebarProduct"
 import ProductCard from "../components/product/ProductCard"
 
 function Shop() {
-  const [product] = useState(useSelector((state) => state.productDate))
+  // url query
+  const useQuery = () => new URLSearchParams(useLocation().search)
+  const query = useQuery()
+
+  // data
+  const [queryString, setQueryString] = useState(query.get("type"))
+  const allProductList = useSelector((state) => state.product)
+
+  // useEffect
+  useEffect(() => {
+    setQueryString(query.get("type"))
+  }, [query, allProductList])
+
+  // function
+  const filterProducts = (queryString, allProductList) => {
+    if (queryString == "all") {
+      return allProductList
+    } else {
+      return allProductList.filter((item) => {
+        return item.id.indexOf(queryString) == 0
+      })
+    }
+  }
+  const queryProducts = filterProducts(queryString, allProductList)
+  // const queryProducts = useMemo(
+  //   () => filterProducts(queryString, allProductList),
+  //   [queryString, allProductList]
+  // )
+
   return (
-    <section className="flex gap-x-4">
+    <section className="flex gap-x-16">
       <section className="hidden lg:block w-1/5">
         <SidebarProduct></SidebarProduct>
       </section>
-      <section className="w-full lg:w-4/5 grid grid-cols-4 gap-x-4 gap-y-8">
-        {product.map((item, index) => (
-          <ProductCard
-            key={index}
-            name={item.name}
-            img={item.img}
-            price={item.price}
-            discount={item.discount}
-          ></ProductCard>
-        ))}
+      <section className="w-full lg:w-4/5">
+        <h3 className="pb-4 mb-8 text-3xl font-semibold border-b border-gray-light">
+          Shop
+        </h3>
+        <section className="grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-x-4 gap-y-8">
+          {queryProducts.map((item, index) => (
+            <ProductCard
+              key={index}
+              id={item.id}
+              name={item.name}
+              img={item.img}
+              price={item.price}
+              discount={item.discount}
+            ></ProductCard>
+          ))}
+        </section>
       </section>
     </section>
   )
