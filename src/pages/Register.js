5608ee313d9c123bac85210c28f6a8b92e3585da
zoe <zoe@zoedeMacBook-Air.local>
2021-11-28 23:30:58 +0800

    新增註冊頁面

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,52 @@
+import { useState } from "react"
+
 function Register() {
+  const [userInfo, setUserInfo] = useState({ name: "", email: "", password: "" })
+
+  const updateField = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value,
+    })
+  }
   return (
-    <section>
-      <div>
-        <p>Username</p>
-        <input type="text"></input>
-      </div>
+    <section className="mx-auto w-11/12 sm:w-1/2 xl:w-1/4 text-base">
+      <p>Username</p>
+      <input
+        type="text"
+        name="name"
+        value={userInfo.name}
+        onChange={updateField}
+        className="w-full mt-2 mb-6 py-2 px-4 bg-gray-light rounded-2xl"
+      ></input>
+
+      <p>Email address</p>
+      <input
+        type="text"
+        name="email"
+        value={userInfo.email}
+        onChange={updateField}
+        className="w-full mt-2 mb-6 py-2 px-4 bg-gray-light rounded-2xl"
+      ></input>
+
+      <p>Password</p>
+      <input
+        type="password"
+        name="password"
+        value={userInfo.password}
+        onChange={updateField}
+        className="w-full mt-2 mb-16 py-2 px-4 bg-gray-light rounded-2xl"
+      ></input>
+
+      <button
+        type="button"
+        className="w-full py-2 bg-black text-white rounded-2xl"
+        onClick={() => {
+          console.log(userInfo)
+        }}
+      >
+        Register
+      </button>
     </section>
   )
 }
